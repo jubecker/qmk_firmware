62e7f5edb447830ec5ef2bfccf6059f71bd765b2
Juergen Becker <ju.becker@gmail.com>
2017-12-30 15:51:40 +0100

    swapped thumb keys raise/lower and gui

--- keyboards/iris/keymaps/beekey/keymap.c
+++ keyboards/iris/keymaps/beekey/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
      LSFT, Z  , X  , C  , V  , B  ,LALT,     RALT, N  , M  ,COMM,DOT ,SLSH,RSFT,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LCTL,LOWR,LGSP,         RGSP,RASE,RCTL
+                       LALT,LGSP,LOWR,         RASE,RGSP,RCTL
     //                `----+----+----'        `----+----+----'
   ),
 
