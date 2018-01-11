f1998120395a0949416e19ee6b817d6d2123358e
Juergen Becker <ju.becker@gmail.com>
2018-01-11 21:53:42 +0100

    fixed delayed space via toggle macro

--- keyboards/iris/keymaps/beekey/config.h
+++ keyboards/iris/keymaps/beekey/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 250
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
