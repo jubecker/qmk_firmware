ee100399ff87c79ea469ef2d78a2a80f61d45b26
Juergen Becker <ju.becker@gmail.com>
2018-02-25 22:10:32 +0100

    iris: redefinded layout

--- keyboards/iris/keymaps/beekey/config.h
+++ keyboards/iris/keymaps/beekey/config.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* Use I2C or Serial, not both */
-
 //#define USE_SERIAL
 #define USE_I2C
 
@@ -33,11 +32,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 250
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+//#undef RGBLED_NUM
+//#define RGBLIGHT_ANIMATIONS
+//#define RGBLED_NUM 12
+//#define RGBLIGHT_HUE_STEP 8
+//#define RGBLIGHT_SAT_STEP 8
+//#define RGBLIGHT_VAL_STEP 8
 
 #endif
