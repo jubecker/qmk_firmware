0ed62e14a554e4ef288062d9278fcafbf49391f3
Juergen Becker <ju.becker@gmail.com>
2019-07-07 13:09:54 +0200

    added 'f' as layer tap key

--- keyboards/playkbtw/ca66/keymaps/beekey/keymap.c
+++ keyboards/playkbtw/ca66/keymaps/beekey/keymap.c
@@ -1,12 +1,5 @@
 /*
-*KeLorean CA66 keymap.
-*Made for HHKB layout with CA66 badge in lower left corner and split spacebars.
-*Left Spacebar - tap for backspace, hold for Layer1
-*Right Spacebar - tap for space, hold for Layer2
-*Layer 2 is same as Layer 1 except Left Spacebar is now delete
-*Cap Locks has been replaced with Momentary Toggle to Layer3
-*Layer 4 can be reached only by Layer3 in the '\' key
-*Layer 4 tab is the soft reset.
+* beekey CA66 keymap
 */
 
 #include QMK_KEYBOARD_H
@@ -16,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 		KC_ESC, 		   KC_1,     KC_2, 	   KC_3, 	KC_4, 	KC_5, 	  KC_6, 	KC_7, 	 KC_8, 		KC_9, 	KC_0, 	 KC_MINS, 	KC_EQL, 	KC_BSPC, KC_BSPC, 		KC_SLEP,
 		KC_TAB, 		   KC_Q, 	 KC_W, 	   KC_E, 	KC_R, 	KC_T, 	  KC_Y, 	KC_U, 	 KC_I, 		KC_O, 	KC_P, 	 KC_LBRC, 	KC_RBRC, 	KC_BSLS, 				KC_PGUP,
-		KC_LCTL, 			   KC_A, 	 KC_S, 	   KC_D, 	KC_F, 	KC_G, 	  KC_H, 	KC_J, 	 KC_K, 		KC_L, 	KC_SCLN, KC_QUOT, 				KC_ENT, 				KC_PGDN,
+		MO(1), 			   KC_A, 	 KC_S, 	   KC_D, 	LT(1, KC_F), 	KC_G, 	  KC_H, 	KC_J, 	 KC_K, 		KC_L, 	KC_SCLN, KC_QUOT, 				KC_ENT, 				KC_PGDN,
 		KC_LSFT, KC_LSFT,  KC_Z, 	 KC_X, 	   KC_C, 	KC_V, 	KC_B, 	  KC_N, 	KC_M, 	 KC_COMM, 	KC_DOT, KC_SLSH, KC_RSFT, 	KC_UP, 								MO(2),
 		KC_LCTL, 			   MO(1),  KC_LALT,  KC_LGUI, 	LT(1, KC_SPC),  	KC_RGUI, KC_RALT,   KC_RCTL, 		   KC_LEFT, KC_DOWN, KC_RGHT),
 
