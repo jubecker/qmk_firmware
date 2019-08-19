a4f6539f79a00d682fd22dcadb6b2e74a8a9a86f
Juergen Becker <ju.becker@gmail.com>
2019-08-19 16:12:44 +0200

    swapped enter and quot

--- keyboards/preonic/keymaps/beekey/keymap.c
+++ keyboards/preonic/keymaps/beekey/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  "   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |Raise |      | Alt  | GUI  |    Space    |Space |   [  |   ]  |  -   |  =   |
  * `-----------------------------------------------------------------------------------'
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_ESC,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
-  KC_LCTL, KC_A,  KC_S,    KC_D,    LT_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,  \
+  KC_LCTL, KC_A,  KC_S,    KC_D,    LT_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
   KC_LCTL, RAISE, ADJUST, KC_LALT, KC_LGUI,  LT_LOWR, LT_LOWR, KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
@@ -63,18 +63,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      | Vol+ |   €  |      |      |      | PgUp | Up   | PgDn |   [  |   ]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Stop | Mute | Play |      |      | Home | Left | Down | Right|  End |   "  |
+ * |      | Stop | Vol- | Play |      |      | Home | Left | Down | Right|  End |   "  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      | Vol- |      |      |      |   ~  |      |      |      |   \  |   |  |
+ * |      |      | Mute |      |      |      |   ~  |      |      |      |   \  |   |  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
-   KC_GRV, KC_EXLM,KC_AT,  KC_HASH,KC_DLR,KC_PERC,KC_CIRC, KC_AMPR, KC_ASTR,KC_MINS,KC_EQL, KC_BSPC, \
+   KC_GRV, KC_EXLM,KC_AT,  KC_HASH,KC_DLR,KC_PERC,KC_CIRC, KC_AMPR, KC_ASTR,KC_MINS,KC_EQL, KC_BSPC,  \
    _______, _______,KC_VOLU,KC_EURO,_______,_______,_______, KC_PGUP,KC_UP,  KC_PGDN,KC_LBRC,KC_RBRC, \
-   _______, KC_MSTP,KC_MUTE,KC_MPLY,_______,_______,KC_HOME, KC_LEFT,KC_DOWN,KC_RGHT,KC_END, KC_QUOT, \
-   _______, _______,KC_VOLD,_______,_______,_______,KC_TILD, _______,_______,_______,KC_BSLS,KC_PIPE, \
+   _______, KC_MSTP,KC_VOLD,KC_MPLY,_______,_______,KC_HOME, KC_LEFT,KC_DOWN,KC_RGHT,KC_END, _______, \
+   _______, _______,KC_MUTE,_______,_______,_______,KC_TILD, _______,_______,_______,KC_BSLS,KC_PIPE, \
    _______, _______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______  \
 ),
 
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5, KC_F6,   KC_F7,   KC_F8, KC_F9,  KC_F10, KC_F11, KC_F12, \
   _______, KC_WAKE,_______,_______, RESET ,_______, _______,_______,_______,_______,_______,_______, \
-  _______, KC_SLEP,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______, \
+  _______, _______,KC_SLEP,_______,_______,_______, _______,_______,_______,_______,_______,_______, \
   _______, _______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______, \
   _______, _______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______  \
 ),
