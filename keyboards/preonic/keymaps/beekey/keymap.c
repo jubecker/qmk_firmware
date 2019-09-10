a8440f73607010dec0fd52da9d5a1815aa4872d3
Juergen Becker <ju.becker@gmail.com>
2019-09-10 19:56:43 +0200

    cleanup the beekey preonic keymap

--- keyboards/preonic/keymaps/beekey/keymap.c
+++ keyboards/preonic/keymaps/beekey/keymap.c
@@ -32,7 +32,7 @@ enum preonic_keycodes {
 
 #define KC_EURO   LSFT(LALT(KC_2))
 #define LT_LOWR   LT(_LOWER, KC_SPC)          // L-ayer T-ap Lower / Space
-#define LT_F   LT(_LOWER, KC_F)          // L-ayer T-ap Lower / F
+#define LT_F      LT(_LOWER, KC_F)          // L-ayer T-ap Lower / F
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Enter|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |Raise |      | Alt  | GUI  |    Space    |Space |   [  |   ]  |  -   |  =   |
  * `-----------------------------------------------------------------------------------'
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_ESC,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
-  KC_LCTL, KC_A,  KC_S,    KC_D,    LT_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL, RAISE, ADJUST, KC_LALT, KC_LGUI,  LT_LOWR, LT_LOWR, KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL, KC_A,  KC_S,    KC_D,    LT_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,  \
+  KC_NO,   RAISE, ADJUST, KC_LALT, KC_LGUI,  LT_LOWR, LT_LOWR, KC_RGUI, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
 ),
 
 /* Lower
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      | Vol+ |   €  |      |      |      | PgUp | Up   | PgDn |   [  |   ]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Stop | Vol- | Play |      |      | Home | Left | Down | Right|  End |   "  |
+ * |      | Stop | Vol- | Play |      |      | Home | Left | Down | Right|  End |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      | Mute |      |      |      |   ~  |      |      |      |   \  |   |  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -71,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
-   KC_GRV, KC_EXLM,KC_AT,  KC_HASH,KC_DLR,KC_PERC,KC_CIRC, KC_AMPR, KC_ASTR,KC_MINS,KC_EQL, KC_BSPC,  \
+   KC_GRV,  KC_EXLM,KC_AT,  KC_HASH,KC_DLR,KC_PERC,KC_CIRC, KC_AMPR, KC_ASTR,KC_MINS,KC_EQL, _______, \
    _______, _______,KC_VOLU,KC_EURO,_______,_______,_______, KC_PGUP,KC_UP,  KC_PGDN,KC_LBRC,KC_RBRC, \
    _______, KC_MSTP,KC_VOLD,KC_MPLY,_______,_______,KC_HOME, KC_LEFT,KC_DOWN,KC_RGHT,KC_END, _______, \
    _______, _______,KC_MUTE,_______,_______,_______,KC_TILD, _______,_______,_______,KC_BSLS,KC_PIPE, \
-   _______, _______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______  \
+   _______, _______,_______,_______,_______,_______, _______,_______,KC_MSTP,KC_VOLU,KC_VOLD,KC_MPLY  \
 ),
 
 /* Raise
