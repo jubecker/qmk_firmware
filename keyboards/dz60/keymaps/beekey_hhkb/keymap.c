152ea9939ce893bf0ce870811a24430a96d33c92
Juergen Becker <ju.becker@gmail.com>
2020-03-01 17:32:30 +0100

    DZ60 HHKB cleanup

--- keyboards/dz60/keymaps/beekey_hhkb/keymap.c
+++ keyboards/dz60/keymaps/beekey_hhkb/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
-#define LT_SPC    LT(1, KC_SPC)          // L-ayer T-ap Lower / Space
-#define LT_F      LT(1, KC_F)          // L-ayer T-ap Lower /F
+#define MT_SPC    MT(KC_LGUI, KC_SPC)
+#define LT_F      LT(1, KC_F)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
 		KC_LCTL, KC_A, KC_S, KC_D, LT_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(2),
-		KC_NO, KC_LALT, KC_LGUI,    KC_SPC, KC_SPC, KC_SPC, KC_SPC,       KC_RGUI, KC_RALT, KC_RALT, KC_NO),
+		KC_LALT, KC_LALT, KC_LGUI,    KC_SPC, KC_SPC, KC_SPC, KC_SPC,       KC_RGUI, KC_RALT, KC_RALT, KC_NO),
 
 	LAYOUT(
 		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______,
@@ -28,5 +28,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {};
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {}
 
